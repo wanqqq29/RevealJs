ff49f2985d139bc56afa8999200e2145f5ba2821
wanqqq29 <wanqqq29@gmail.com>
2022-01-05 09:52:09 +0800

    样式出问题

--- revealJs/src/components/preview.vue
+++ revealJs/src/components/preview.vue
@@ -1,27 +1,53 @@
 <!--
  * @Author: wanqqq29
  * @Date: 2022-01-04 21:54:50
- * @LastEditTime: 2022-01-04 22:08:19
+ * @LastEditTime: 2022-01-04 23:58:23
  * @LastEditors: wanqqq29
  * @Description: blog.wanqqq29.cn
  * @FilePath: \RevealJs\revealJs\src\components\preview.vue
 -->
-<template>This is Preview.vue</template>
+<template>
+  This is Preview.vue
+  <div class="reveal" id="content">>
+    <section data-markdown>
+      <textarea data-template>
+    ## Slide 1
+    A paragraph with some text and a [link](http://hakim.se).
+    ---
+    ## Slide 2
+    ---
+    ## Slide 3
+  </textarea
+      >
+    </section>
+  </div>
+</template>
+
+<style>
+@import url("/node_modules/reveal.js/dist/reveal.css");
+@import url("/node_modules/reveal.js/dist/theme/black.css");
+</style>
 
 <script>
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-
-
+import { onMounted } from "vue";
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import RevealMarkdown from "reveal.js/plugin/markdown/markdown.js";
 
 export default {
   name: "preview",
 
   setup() {
+    const init = () => {
       let deck = new Reveal({
-   plugins: [ Markdown ]
-})
-deck.initialize();
+        plugins: [RevealMarkdown],
+      });
+      deck.initialize();
+    };
+
+    onMounted(() => {
+      init();
+    });
   },
 };
 </script>
