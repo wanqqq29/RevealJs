f9d0f554b70dbcc21c1f9a66e17e492d1128dcc2
wanqqq29 <wanqqq29@gmail.com>
2022-01-07 17:28:08 +0800

    bug:无法所见即所得

--- revealJs/src/pages/test.vue
+++ revealJs/src/pages/test.vue
@@ -1,31 +1,71 @@
 <!--
  * @Author: wanqqq29
  * @Date: 2022-01-04 21:51:48
- * @LastEditTime: 2022-01-06 15:48:01
+ * @LastEditTime: 2022-01-07 15:42:57
  * @LastEditors: wanqqq29
  * @Description: blog.wanqqq29.cn
- * @FilePath: \RevealJs\revealJs\src\pages\test.vue
+ * @FilePath: \revealJs\src\pages\test.vue
 -->
 <template>
-  <div class="row">
-    <div class="col-6">
-      <edit />
+  <div class="row mainbox">
+    <div class="col-6 cRight">
+      This is Edit.vue
+      <textarea name="md_data" v-model="inp_md_data"> </textarea>
     </div>
-    <div class="col-6">
-      <preview />
+    <div class="to" @click="BeginReveal">></div>
+    <div class="col-6 cLeft">
+      This is Preview.vue
+      <div class="reveal" id="content">
+        <div class="slides">
+          <section data-markdown>
+            {{ inp_md_data }}
+          </section>
+          <section data-markdown>## 2</section>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<style scoop src="../../node_modules/reveal.js/dist/reveal.css"></style>
+<style scoop src="../../node_modules/reveal.js/dist/theme/black.css"></style>
 <style lang="scss" scoped src="../css/edit.scss"></style>
-
 <script>
-import edit from "../components/edit.vue";
-import preview from "../components/preview.vue";
+import { ref, onMounted, computed, reactive } from "vue";
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+
 export default {
-  components: {
-    edit: edit,
-    preview: preview,
+  setup() {
+    let reveal = reactive(Object);
+
+    const init = () => {
+      reveal = new Reveal();
+    };
+
+    const inp_md_data = ref(
+      
+    );
+
+    const BeginReveal = () => (
+      reveal.initialize({
+        plugins: [Markdown],
+      }),
+      console.log("11")
+    );
+
+    const comput_md_data = computed(() => {});
+    console.log(comput_md_data.value);
+
+    onMounted(() => {
+      init();
+    });
+    return {
+      reveal,
+      inp_md_data,
+      comput_md_data,
+      init,
+      BeginReveal,
+    };
   },
-  setup() {},
 };
 </script>
